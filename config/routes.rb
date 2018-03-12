6aa488afcf5c102274587ad8b0cb6732d135387d
Arpita Rathore <arpitarathore.ar@gmail.com>
2018-03-12 11:09:15 -0400

    shorten path fixed

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
   root 'home#index'
 
-  get '/:shorten', to: 'home#visit'
+  get '/:shorten', to: 'home#visit', as: :visit
 
   resources :links
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
