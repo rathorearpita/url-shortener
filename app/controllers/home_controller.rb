1edb4e9fb9267d8164479f75f4e3b025327062f1
Arpita Rathore <arpitarathore.ar@gmail.com>
2018-03-12 10:45:53 -0400

    Track Clicks

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,11 +1,12 @@
 class HomeController < ApplicationController
   def index
     @link = Link.new
-    @top_links = Link.order(clicks: :desc).first(15)
+    @top_links = Link.order(clicks: :desc).first(10)
   end
 
   def visit
     link = Link.find_by!(shorten: params[:shorten])
+    link.increment!(:clicks)
     redirect_to link.url
   end
 end
